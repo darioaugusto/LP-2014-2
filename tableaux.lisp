8ddc75d59f6a331a9723c7d72580b38931760e5c
Alexandre Rademaker <arademaker@gmail.com>
2014-11-21 21:00:23 -0200

    finished tableaux

--- tableaux.lisp
+++ tableaux.lisp
@@ -6,9 +6,15 @@
   ((sign    :initform nil :initarg :sign :accessor formula-sign)
    (frm     :initform nil :initarg :frm  :accessor formula-frm)))
 
+(defmethod print-object ((frm formula) stream)
+  (format stream "[~a] ~a" (formula-sign frm) (formula-frm frm)))
+
 
 (defun atomic? (formula)
   (not (listp (formula-frm formula))))
+
+(defun invert-sign (sign)
+  (if (equal sign 'true) 'false 'true))
 
 (defun make-formula (sign wff)
   (make-instance 'formula :sign sign :frm wff))
@@ -20,54 +26,96 @@
   (equal sign (formula-sign formula)))
 
 
-(defun crash? (branch)
-  (let ((atoms (remove-if-not #'atomic? branch)))
-    (loop for atom = (car atoms) then (car others)
-	  for others = (cdr atoms) then (cdr others)
-	  (if (remove-if-not (lambda (o-frm)
-			       (and (not (sign? (formula-sign atom) o-frm))
-				    (equal (formula-frm atom) 
-					   (formula-frm o-frm))))
-			     others)))))
+(defun cost (a-formula)
+  " sign = true or false; op = and or implies not "
+  (let ((op (car (formula-frm a-formula)))
+	(sign (formula-sign a-formula))) 
+    (case op
+      (and     (case sign (true 1) (false 2)))
+      (or      (case sign (true 2) (false 1)))
+      (implies (case sign (true 2) (false 1)))
+      (not 1)
+      (otherwise nil))))
 
 
-(defun expand (formula branch)
-  (cond
-    ((and (is? 'and formula) (sign? 'f formula)) 
-     (let ((lhs (make-formula 'f (cadr  wff)))
-	   (rhs (make-formula 'f (caddr wff))))
-       (list (cons lhs branch) (cons rhs branch))))
-    ((and (is? 'and formula) (sign? 't formula)) 
-     (let ((lhs (make-formula 't (cadr  wff)))
-	   (rhs (make-formula 't (caddr wff))))
-       (list (append (list lhs rhs) branch))))
-    ((and (is? 'or formula) (sign? 't formula))
-     (let ((lhs (make-formula 't (cadr  wff)))
-	   (rhs (make-formula 't (caddr wff))))
-       (list (cons lhs branch) (cons rhs branch))))
-    ((and (is? 'or formula) (sign? 'f formula))
-     (let ((lhs (make-formula 'f (cadr  wff)))
-	   (rhs (make-formula 'f (caddr wff))))
-       (list (cons lhs branch) (cons rhs branch))))
-    ((and (is? 'not formula) (sign? 'f formula)) 
-     (list (cons (make-formula 'f (cadr wff)) branch)))
-    ((and (is? 'not formula) (sign? 't formula)) 
-     (list (cons (make-formula 't (cadr wff)) branch)))
-    (t nil)))
+(defun derive (branch)
+  (if (remove-if #'atomic? branch)
+      (let* ((frms (sort (remove-if #'atomic? branch) 
+			 #'< :key #'cost))
+	     (rest (remove (car frms) branch :test #'equal))) 
+	(values (apply-rule (car frms)) rest))
+      (values nil branch)))
 
 
-(defun solve (branches)
-  (let ((non-atomics (remove-if #'atomic? (car branches)))
-	(new-branches nil)) 
-    (if (null non-atomics)
-	non-atomics
-	(let ((a-form (car non-atomics)))
-	  (let ((news (expand a-form (car branches)))) 
-	    (dolist (n news)
-	      (if (non-crash? n)
-		  (push n new-branches)))
-	    (solve new-branches))))))
+(defun apply-rule (formula)
+  "Given a branch, select a formula to be decomposed according the rules."
+  (labels ((beta (s1 s2 wff)
+	     (list (list (make-formula s1 (cadr  wff))) 
+		   (list (make-formula s2 (caddr wff)))))
+	   (alfa (s1 s2 wff)
+	     (list (list (make-formula s1 (cadr  wff))
+			 (make-formula s2 (caddr wff))))))
+    (let ((wff (formula-frm formula))) 
+    (cond
+      ((and (is? 'and formula) (sign? 'false formula)) 
+       (beta 'false 'false wff))
+      ((and (is? 'and formula) (sign? 'true formula)) 
+       (alfa 'true 'true wff))
+      ((and (is? 'or formula) (sign? 'true formula))
+       (beta 'true 'true wff))
+      ((and (is? 'or formula) (sign? 'false formula))
+       (alfa 'false 'false wff))
+      ((and (is? 'implies formula) (sign? 'false formula))
+       (alfa 'true 'false wff))
+      ((and (is? 'implies formula) (sign? 'true formula))
+       (beta 'false 'true wff))
+      ((is? 'not formula) 
+       (let ((ns (invert-sign (formula-sign formula)))) 
+	 (list (list (make-formula ns (cadr wff))))))
+      (t nil)))))
+
+
+(defun unify (frm1 frm2)
+  (and (atomic? frm2)
+       (atomic? frm1)
+       (equal (invert-sign (formula-sign frm1))
+	      (formula-sign frm2))
+       (equal (formula-frm frm1) 
+	      (formula-frm frm2))))
+
+
+(defun full-expanded? (branch)
+  (every #'atomic? branch))
+
+
+(defun expand-branch (frms branch)
+  (if (null frms)  
+      branch
+      (let ((frm (car frms))
+	    (res (cdr frms)))
+	(if (find frm branch :test #'unify)
+	    nil
+	    (expand-branch res (cons frm branch))))))
+
+
+(defun expand-branches (lolf branch branches)
+  (if (null lolf) 
+      branches
+      (let ((newb (expand-branch (car lolf) branch)))
+	(if newb 
+	    (expand-branches (cdr lolf) branch (cons newb branches))
+	    (expand-branches (cdr lolf) branch branches)))))
+
+
+(defun prove-step (branches)
+  (multiple-value-bind (news branch-rest)
+      (derive (car branches))
+    (expand-branches news branch-rest (cdr branches))))
 
 
 (defun prove (wff)
-  (solve (list (list (make-formula 'f wff)))))
+  (do ((branches (list (list (make-formula 'false wff)))
+		 (prove-step branches)))
+      ((or (null branches)
+	   (every #'full-expanded? branches)) 
+       branches)))
